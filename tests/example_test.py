8843325b02bf763b0783466dfbc741e612dd8fa8
tomcatling <tomcatling@gmail.com>
2019-02-01 17:06:23 +0000

    Update example_test.py

--- tests/example_test.py
+++ tests/example_test.py
@@ -1,1 +1,14 @@
-import black_nb
+import imp
+black_nb = imp.load_source('black-nb', '../black-nb')
+
+import click
+from click.testing import CliRunner
+
+def test_hello_world():
+    runner = CliRunner()
+    result = runner.invoke(black_nb.main, ['.'])
+    #assert result.exit_code == 0
+    #assert result.output == 'Hello Peter!\n'
+    return result
+    
+test_hello_world()
