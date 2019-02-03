4cc36df79b5dd5a9683a4b3f7d43ec2898f0e377
tomcatling <tomcatling@gmail.com>
2019-02-03 14:22:33 +0000

    testing with pytest

--- tests/example_test.py
+++ tests/example_test.py
@@ -1,11 +1,7 @@
 import black_nb
-import click
 from click.testing import CliRunner
 
 def test_basic():
     runner = CliRunner()
     result = runner.invoke(black_nb.main, ['.'])
     assert result.exit_code == 0
-    
-if __name__ == "__main__":
-    test_basic()
