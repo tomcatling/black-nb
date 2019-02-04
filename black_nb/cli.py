c4b090081fe3f4d442d3ea6a81d8fae59d9097fa
Liam Coatman <liam.coatman@gmail.com>
2019-02-04 17:05:29 +0000

    WIP

--- black_nb/cli.py
+++ black_nb/cli.py
@@ -1,5 +1,19 @@
 """Apply Black to Jupyter notebooks."""
 
+# Copyright © 2019 Tom Catling <tomcatling@gmail.com>
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+# REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+# AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+# INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+# LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+# OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+# PERFORMANCE OF THIS SOFTWARE.
+
 import re
 from pathlib import Path
 from typing import Any, Dict, List, Optional, Set, Tuple
@@ -91,7 +105,7 @@ DEFAULT_EXCLUDES = (
     help="Read configuration from PATH.",
 )
 @click.pass_context
-def main(
+def cli(
     ctx: click.Context,
     line_length: int,
     check: bool,
@@ -434,4 +448,4 @@ class SubReport:
 
 
 if __name__ == "__main__":
-    main()
+    cli()
---
+++ setup.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+
+from setuptools import setup, find_packages
+
+
+def read_long_description() -> str:
+    """Read from README.md file in root of source directory."""
+    root = Path(__file__).resolve().parent
+    readme = root / "README.md"
+    return readme.read_text(encoding="utf-8")
+
+
+setup(
+    name="black-nb",
+    description="Apply black to all code cells in a Jupyter notebook.",
+    long_description=read_long_description(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/tomcatling/black-nb",
+    author="Tom Catling",
+    author_email="tomcatling@gmail.com>",
+    license="ISC",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Operating System :: Unix",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: ISC License (ISCL)",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=find_packages(),
+    use_scm_version={"version_scheme": "post-release"},
+    setup_requires=["setuptools_scm"],
+    python_requires='>3.7',
+    install_requires=[
+        "black==18.9b0", "click", "nbformat"
+    ],
+    entry_points={"console_scripts": ["black-nb=black_nb.cli:cli"]},
+)
