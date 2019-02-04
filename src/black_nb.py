cb375f10d1e2091c1cad57a4157f59a12e5f227d
tomcatling <tomcatling@gmail.com>
2019-02-04 00:23:16 +0000

    mypy generic type error

--- src/black_nb.py
+++ src/black_nb.py
@@ -1,11 +1,13 @@
+"""Apply Black to Jupyter notebooks."""
+
 import re
 from pathlib import Path
+from dataclasses import dataclass
 from typing import Any, Dict, List, Optional, Set, Tuple
 
 import black
 import click
 import nbformat
-from dataclasses import dataclass
 
 DEFAULT_LINE_LENGTH = 79
 DEFAULT_INCLUDES = r"\.ipynb$"
