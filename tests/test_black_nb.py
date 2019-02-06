3606e3c8ffb2c6977ddd252aec090f495b63a83e
Liam Coatman <liam.coatman@gmail.com>
2019-02-06 16:36:38 +0000

    Removing typing

--- tests/test_black_nb.py
+++ tests/test_black_nb.py
@@ -5,6 +5,7 @@ from typing import List
 import black
 from black_nb.cli import DEFAULT_EXCLUDES, DEFAULT_INCLUDES, cli
 from click.testing import CliRunner
+import pytest
 
 THIS_FILE = Path(__file__)
 THIS_DIR = THIS_FILE.parent
@@ -16,25 +17,23 @@ def test_basic():
     assert result.exit_code == 0
 
 
-def test_include_exclude() -> None:
+def test_include_exclude():
     path = THIS_DIR / "data" / "include_exclude_tests"
     include = re.compile(r"\.ipynb$")
     exclude = re.compile(r"/exclude/|/\.definitely_exclude/")
     report = black.Report()
-    sources: List[Path] = []
     expected = [Path(path / "b/dont_exclude/a.ipynb")]
     this_abs = THIS_DIR.resolve()
-    sources.extend(
-        black.gen_python_files_in_dir(path, this_abs, include, exclude, report)
+    sources = black.gen_python_files_in_dir(
+        path, this_abs, include, exclude, report
     )
     assert sorted(expected) == sorted(sources)
 
 
-def test_empty_include() -> None:
+def test_empty_include():
     path = THIS_DIR / "data" / "include_exclude_tests"
     report = black.Report()
     empty = re.compile(r"")
-    sources: List[Path] = []
     expected = [
         Path(path / "b/exclude/a.p"),
         Path(path / "b/exclude/a.py"),
@@ -47,34 +46,29 @@ def test_empty_include() -> None:
         Path(path / "b/.definitely_exclude/a.ipynb"),
     ]
     this_abs = THIS_DIR.resolve()
-    sources.extend(
-        black.gen_python_files_in_dir(
-            path, this_abs, empty, re.compile(DEFAULT_EXCLUDES), report
-        )
+    sources = black.gen_python_files_in_dir(
+        path, this_abs, empty, re.compile(DEFAULT_EXCLUDES), report
     )
     assert sorted(expected) == sorted(sources)
 
 
-def test_empty_exclude() -> None:
+def test_empty_exclude():
     path = THIS_DIR / "data" / "include_exclude_tests"
     report = black.Report()
     empty = re.compile(r"")
-    sources: List[Path] = []
     expected = [
         Path(path / "b/dont_exclude/a.ipynb"),
         Path(path / "b/exclude/a.ipynb"),
         Path(path / "b/.definitely_exclude/a.ipynb"),
     ]
     this_abs = THIS_DIR.resolve()
-    sources.extend(
-        black.gen_python_files_in_dir(
-            path, this_abs, re.compile(DEFAULT_INCLUDES), empty, report
-        )
+    sources = black.gen_python_files_in_dir(
+        path, this_abs, re.compile(DEFAULT_INCLUDES), empty, report
     )
     assert sorted(expected) == sorted(sources)
 
 
-def test_invalid_include_exclude() -> None:
+def test_invalid_include_exclude():
     for option in ["--include", "--exclude"]:
         result = CliRunner().invoke(cli, ["-", option, "**()(!!*)"])
         assert result.exit_code == 2
