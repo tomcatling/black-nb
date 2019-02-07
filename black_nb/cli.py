0d44b74f8e2c00c111509c5a6b0d0ed62886cb4d
Liam Coatman <liam.coatman@gmail.com>
2019-02-07 16:55:57 +0000

    Delete old commented out lines

--- black_nb/cli.py
+++ black_nb/cli.py
@@ -287,9 +287,6 @@ def format_file_in_place(
         dst_cells.append(cell)
     src_contents["cells"] = dst_cells
 
-    # click.echo(src)
-    # click.secho(f"    {sub_report}", err=True)
-
     if write_back is black.WriteBack.YES:
         with src.open("w") as fp:
             nbformat.write(src_contents, fp)
