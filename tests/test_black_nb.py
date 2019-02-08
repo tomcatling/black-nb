5d87706889259c9971ebab3bc08374de1998e4f4
Liam Coatman <liam.coatman@gmail.com>
2019-02-08 19:16:58 +0000

    Remove redundant tests

--- tests/test_black_nb.py
+++ tests/test_black_nb.py
@@ -54,57 +54,6 @@ def test_invalid_input(tmp_path):
     assert CliRunner().invoke(cli, [str(dst_dir)]).exit_code == 123
 
 
-def test_include_exclude():
-    path = THIS_DIR / "data" / "include_exclude_tests"
-    include = re.compile(r"\.ipynb$")
-    exclude = re.compile(r"/exclude/|/\.definitely_exclude/")
-    report = black.Report()
-    expected = [Path(path / "b/dont_exclude/a.ipynb")]
-    this_abs = THIS_DIR.resolve()
-    sources = black.gen_python_files_in_dir(
-        path, this_abs, include, exclude, report
-    )
-    assert sorted(expected) == sorted(sources)
-
-
-def test_empty_include():
-    path = THIS_DIR / "data" / "include_exclude_tests"
-    report = black.Report()
-    empty = re.compile(r"")
-    expected = [
-        Path(path / "b/exclude/a.p"),
-        Path(path / "b/exclude/a.py"),
-        Path(path / "b/exclude/a.ipynb"),
-        Path(path / "b/dont_exclude/a.p"),
-        Path(path / "b/dont_exclude/a.py"),
-        Path(path / "b/dont_exclude/a.ipynb"),
-        Path(path / "b/.definitely_exclude/a.p"),
-        Path(path / "b/.definitely_exclude/a.py"),
-        Path(path / "b/.definitely_exclude/a.ipynb"),
-    ]
-    this_abs = THIS_DIR.resolve()
-    sources = black.gen_python_files_in_dir(
-        path, this_abs, empty, re.compile(DEFAULT_EXCLUDES), report
-    )
-    assert sorted(expected) == sorted(sources)
-
-
-def test_empty_exclude():
-    path = THIS_DIR / "data" / "include_exclude_tests"
-    report = black.Report()
-    empty = re.compile(r"")
-    expected = [
-        Path(path / "b/dont_exclude/a.ipynb"),
-        Path(path / "b/exclude/a.ipynb"),
-        Path(path / "b/.definitely_exclude/a.ipynb"),
-    ]
-    this_abs = THIS_DIR.resolve()
-    sources = black.gen_python_files_in_dir(
-        path, this_abs, re.compile(DEFAULT_INCLUDES), empty, report
-    )
-    assert sorted(expected) == sorted(sources)
-
-
 @pytest.mark.parametrize("option", ["--include", "--exclude"])
 def test_invalid_include_exclude(option):
     result = CliRunner().invoke(cli, [option, "**()(!!*)"])
