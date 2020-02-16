10330b1f11cda2728503f83c2de195e2c3fd7ad2
Liam Coatman <liam.coatman@gmail.com>
2020-02-16 11:08:19 +0000

    Update copywright to 2020

--- black_nb/cli.py
+++ black_nb/cli.py
@@ -1,7 +1,7 @@
 """Apply Black to Jupyter notebooks."""
 
 # Original work Copyright © 2018 Łukasz Langa
-# Modified work Copyright © 2019 Tom Catling, Liam Coatman
+# Modified work Copyright © 2020 Tom Catling, Liam Coatman
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
